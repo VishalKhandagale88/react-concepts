3642b9c7bbaeb9214caa436fa9c72f8002c7bef4
vishal <vishal88kh@gmail.com>
2023-10-13 20:27:46 +0530

    learned about ternary operators

--- react-basics/src/App.tsx
+++ react-basics/src/App.tsx
@@ -1,10 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-
+import "./App.css" 
 
 function App() {
   
   const myName = "vishal";
   const appName = <h2>React</h2>
+
+  const age =15;
+  const isGreen = "true"
   
   return (
     <>
@@ -16,6 +19,14 @@ function App() {
       <Job salary={20000} position="SDE" company="Google" />
       <Job salary={10000} position="full-stack-developer" company="Amazon" />
       <Job salary={20022} position = "consultant" company="Instgram" />
+      <div className="second-div">
+      </div>
+      <h1>Ternary operators,Lists and css</h1>
+      <div>
+        {age>=18 ? <h1>Over age</h1> : <h1>under age</h1> }
+        <h1 style={{color: isGreen ? "green" : "white",backgroundColor:"gold"}}>This text red color</h1>
+        {isGreen && <button>This is button</button> }
+      </div>
     </>
   )
 }
