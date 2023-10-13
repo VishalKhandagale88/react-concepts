2fdaf39ce3e0aa2c7feb3f8a09d4dec415a326e6
vishal <vishal88kh@gmail.com>
2023-10-13 22:08:57 +0530

    added a state change method to change the input value

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,12 +1,19 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-
-
+  const[todolist,setToDoList] = useState([]);
+  const[inputValue,setInputValue] = useState("");
+  const changeInput = (event)=>{
+    setInputValue(event.target.value);
+  }
   return (
     <>
      <div className='App'>
-      <div className='addTask'></div>
+      <div className='addTask'>
+        <input onChange={changeInput} type="text" />
+        <button  >Add Task</button>
+      </div>
       <div className='list'></div>
      </div>
     </>
