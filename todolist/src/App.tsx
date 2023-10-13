89421fadb274f17ec1c02ae6f10557c2c05b700c
vishal <vishal88kh@gmail.com>
2023-10-13 22:17:40 +0530

    completed functionality to add task in the list

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -7,14 +7,24 @@ function App() {
   const changeInput = (event)=>{
     setInputValue(event.target.value);
   }
+  const addTask = () =>{
+    const newToDoList = [...todolist,inputValue];
+    setToDoList(newToDoList);
+  }
   return (
     <>
      <div className='App'>
       <div className='addTask'>
         <input onChange={changeInput} type="text" />
-        <button  >Add Task</button>
+        <button onClick={addTask} >Add Task</button>
       </div>
-      <div className='list'></div>
+      <div className='list'>
+        {todolist.map((task,key)=>{
+          return(
+            <h3 key={key}>{task}</h3>
+          )
+        })}
+      </div>
      </div>
     </>
   )
