e6d77796fcb01e0fde2925d4b268fe6cd0abbc48
vishal <vishal88kh@gmail.com>
2023-10-13 19:54:53 +0530

    learned about the functions and Component differeence

--- react-basics/src/App.tsx
+++ react-basics/src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 
 
 function App() {
@@ -13,5 +14,18 @@ function App() {
     </>
   )
 }
+
+const getName = () =>{
+  return "vishal";
+}
+
+// below one is a react component which returns a jsx 
+// components always having namig convention which is PascalCase
+const GetNameComponent = () =>{
+  return(
+    <h1>getNameComponent</h1>
+  )
+}
+
 
 export default App
